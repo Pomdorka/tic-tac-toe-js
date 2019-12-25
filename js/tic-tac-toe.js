454c64a80d1f79b793d122661b6f90f6ba534b68
Pomdorka <andreychetvertakov@mail.ru>
2019-12-25 20:21:32 +0300

    Update tic-tac-toe.js

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,49 +1,22 @@
-/**
- * Tic Tac Toe
- *
- * A Tic Tac Toe game in HTML/JavaScript/CSS.
- *
- * No dependencies - Uses Vanilla JS
- *
- * @author: Vasanth Krishnamoorthy
- */
 var N_SIZE = 3,
   EMPTY = '&nbsp;',
   boxes = [],
   turn = 'X',
-  score,
-  moves;
+  moves = 0;
 
-/**
- * Initializes the Tic Tac Toe board and starts the game.
- */
 function init() {
   var board = document.createElement('table');
   board.setAttribute('border', 1);
   board.setAttribute('cellspacing', 0);
 
-  var identifier = 1;
   for (var i = 0; i < N_SIZE; i++) {
     var row = document.createElement('tr');
     board.appendChild(row);
     for (var j = 0; j < N_SIZE; j++) {
       var cell = document.createElement('td');
-      cell.setAttribute('height', 120);
-      cell.setAttribute('width', 120);
-      cell.setAttribute('align', 'center');
-      cell.setAttribute('valign', 'center');
-      cell.classList.add('col' + j, 'row' + i);
-      if (i == j) {
-        cell.classList.add('diagonal0');
-      }
-      if (j == N_SIZE - i - 1) {
-        cell.classList.add('diagonal1');
-      }
-      cell.identifier = identifier;
       cell.addEventListener('click', set);
       row.appendChild(cell);
       boxes.push(cell);
-      identifier += identifier;
     }
   }
 
@@ -51,14 +24,7 @@ function init() {
   startNewGame();
 }
 
-/**
- * New game
- */
 function startNewGame() {
-  score = {
-    'X': 0,
-    'O': 0
-  };
   moves = 0;
   turn = 'X';
   boxes.forEach(function (square) {
@@ -66,52 +32,32 @@ function startNewGame() {
   });
 }
 
-/**
- * Check if a win or not
- */
-function win(clicked) {
-  // Get all cell classes
-  var memberOf = clicked.className.split(/\s+/);
-  for (var i = 0; i < memberOf.length; i++) {
-    var testClass = '.' + memberOf[i];
-    var items = contains('#tictactoe ' + testClass, turn);
-    // winning condition: turn == N_SIZE
-    if (items.length == N_SIZE) {
-      return true;
-    }
-  }
-  return false;
+function win() {
+  return (boxes[0].innerHTML == turn && boxes[1].innerHTML == turn && boxes[2].innerHTML == turn) ||
+ 		(boxes[3].innerHTML == turn && boxes[4].innerHTML == turn && boxes[5].innerHTML == turn) ||
+ 		(boxes[6].innerHTML == turn && boxes[7].innerHTML == turn && boxes[8].innerHTML == turn) ||
+ 		(boxes[0].innerHTML == turn && boxes[3].innerHTML == turn && boxes[6].innerHTML == turn) ||
+ 		(boxes[1].innerHTML == turn && boxes[4].innerHTML == turn && boxes[7].innerHTML == turn) ||
+ 		(boxes[2].innerHTML == turn && boxes[5].innerHTML == turn && boxes[8].innerHTML == turn) ||
+		(boxes[0].innerHTML == turn && boxes[4].innerHTML == turn && boxes[8].innerHTML == turn) ||
+		(boxes[2].innerHTML == turn && boxes[4].innerHTML == turn && boxes[6].innerHTML == turn);
 }
 
-/**
- * Helper function to check if NodeList from selector has a particular text
- */
-function contains(selector, text) {
-  var elements = document.querySelectorAll(selector);
-  return [].filter.call(elements, function (element) {
-    return RegExp(text).test(element.textContent);
-  });
-}
-
-/**
- * Sets clicked square and also updates the turn.
- */
 function set() {
   if (this.innerHTML !== EMPTY) {
     return;
   }
   this.innerHTML = turn;
   moves += 1;
-  score[turn] += this.identifier;
-  if (win(this)) {
-    alert('Winner: Player ' + turn);
+  if (win()) {
+    alert('Winner: turn ' + turn);
     startNewGame();
   } else if (moves === N_SIZE * N_SIZE) {
     alert('Draw');
     startNewGame();
   } else {
     turn = turn === 'X' ? 'O' : 'X';
-    document.getElementById('turn').textContent = 'Player ' + turn;
+    document.getElementById('turn').textContent = 'turn ' + turn;
   }
 }
 
