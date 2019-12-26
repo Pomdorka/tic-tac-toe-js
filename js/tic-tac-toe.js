d24bb2b6ac2c7ff63828675b9215c1237fe35498
Pomdorka <andreychetvertakov@mail.ru>
2019-12-26 14:47:13 +0300

    Update tic-tac-toe.js

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -2,63 +2,114 @@ var N_SIZE = 3,
   EMPTY = '&nbsp;',
   boxes = [],
   turn = 'X',
+  parentTable,
   moves = 0;
 
 function init() {
-  var board = document.createElement('table');
-  board.setAttribute('border', 1);
-  board.setAttribute('cellspacing', 0);
+  parentTable = document.createElement('table');
+  
+  parentTable.setAttribute('border', 1);
+  parentTable.setAttribute('cellspacing', 0);
 
-  for (var i = 0; i < N_SIZE; i++) {
-    var row = document.createElement('tr');
-    board.appendChild(row);
-    for (var j = 0; j < N_SIZE; j++) {
-      var cell = document.createElement('td');
-      cell.addEventListener('click', set);
-      row.appendChild(cell);
-      boxes.push(cell);
+  for (var pi = 0; pi < N_SIZE; pi++) {
+    var parentRow = document.createElement('tr');
+    parentTable.appendChild(parentRow);
+    for (var pj = 0; pj < N_SIZE; pj++) {
+      var parentCell = document.createElement('td');
+      parentCell.classList.add("parentTd", 'pi' + pi, 'pj' + pj, "enabled");
+      //parentCell.addEventListener('click', clickEvent);
+      parentRow.appendChild(parentCell);
+
+	  var table = document.createElement('table');
+	  for (var i = 0; i < N_SIZE; i++) {
+		var row = document.createElement('tr');
+		table.appendChild(row);
+		for (var j = 0; j < N_SIZE; j++) {
+		  var cell = document.createElement('td');
+		  cell.classList.add("childTd", 'i' + i, 'j' + j);
+		  cell.addEventListener('click', clickEvent);
+		  row.appendChild(cell);
+		}
+	  }
+	  parentCell.appendChild(table);
     }
   }
-
-  document.getElementById('tictactoe').appendChild(board);
+  document.getElementById('tictactoe').appendChild(parentTable);
   startNewGame();
 }
 
 function startNewGame() {
   moves = 0;
   turn = 'X';
-  boxes.forEach(function (square) {
-    square.innerHTML = EMPTY;
+  Array.from(document.getElementsByClassName("childTd")).forEach(function (childTd) {
+    childTd.innerHTML = EMPTY;
   });
 }
 
-function win() {
-  return (boxes[0].innerHTML == turn && boxes[1].innerHTML == turn && boxes[2].innerHTML == turn) ||
- 		(boxes[3].innerHTML == turn && boxes[4].innerHTML == turn && boxes[5].innerHTML == turn) ||
- 		(boxes[6].innerHTML == turn && boxes[7].innerHTML == turn && boxes[8].innerHTML == turn) ||
- 		(boxes[0].innerHTML == turn && boxes[3].innerHTML == turn && boxes[6].innerHTML == turn) ||
- 		(boxes[1].innerHTML == turn && boxes[4].innerHTML == turn && boxes[7].innerHTML == turn) ||
- 		(boxes[2].innerHTML == turn && boxes[5].innerHTML == turn && boxes[8].innerHTML == turn) ||
-		(boxes[0].innerHTML == turn && boxes[4].innerHTML == turn && boxes[8].innerHTML == turn) ||
-		(boxes[2].innerHTML == turn && boxes[4].innerHTML == turn && boxes[6].innerHTML == turn);
+function win(table) {
+    //console.log(table.rows[0].cells[0]);
+  return (table.rows[0].cells[0].innerHTML == turn && table.rows[0].cells[1].innerHTML == turn && table.rows[0].cells[2].innerHTML == turn) ||
+ 	     (table.rows[1].cells[0].innerHTML == turn && table.rows[1].cells[1].innerHTML == turn && table.rows[1].cells[2].innerHTML == turn) ||
+ 		 (table.rows[2].cells[0].innerHTML == turn && table.rows[2].cells[1].innerHTML == turn && table.rows[2].cells[2].innerHTML == turn) ||
+ 		 (table.rows[0].cells[0].innerHTML == turn && table.rows[1].cells[0].innerHTML == turn && table.rows[2].cells[0].innerHTML == turn) ||
+ 		 (table.rows[0].cells[1].innerHTML == turn && table.rows[1].cells[1].innerHTML == turn && table.rows[2].cells[1].innerHTML == turn) ||
+ 		 (table.rows[0].cells[2].innerHTML == turn && table.rows[1].cells[2].innerHTML == turn && table.rows[2].cells[2].innerHTML == turn) ||
+		 (table.rows[0].cells[0].innerHTML == turn && table.rows[1].cells[1].innerHTML == turn && table.rows[2].cells[2].innerHTML == turn) ||
+		 (table.rows[0].cells[2].innerHTML == turn && table.rows[1].cells[1].innerHTML == turn && table.rows[2].cells[0].innerHTML == turn);
 }
 
-function set() {
+function clickEvent() {
+    //console.log(turn);
   if (this.innerHTML !== EMPTY) {
     return;
   }
-  this.innerHTML = turn;
-  moves += 1;
-  if (win()) {
-    alert('Winner: turn ' + turn);
-    startNewGame();
-  } else if (moves === N_SIZE * N_SIZE) {
-    alert('Draw');
-    startNewGame();
-  } else {
+  if (this.parentNode.parentNode.parentNode.classList.contains("disabled")){
+      return;
+  }
+  
+  
+  
+  
+  //console.log(pi + ' ' + pj);
+ 
+  //console.log('after: ' + parentTd.classList);
+    this.innerHTML = turn;
+    if (win(this.parentNode.parentNode)){
+        this.parentNode.parentNode.parentNode.innerHTML = turn;
+    }
+    if (win(parentTable)){
+        alert("win: " + turn);
+        startNewGame();
+    }
+    
+    
+    
+    
+    var pi, pj;
+      var memberOf = this.className.split(/\s+/);
+      for (var k = 0; k < memberOf.length; k++){
+          if (memberOf[k].startsWith('i')){
+              pi = 'pi' + memberOf[k].substr(1);
+          }
+          if (memberOf[k].startsWith('j')){
+              pj = 'pj' + memberOf[k].substr(1);
+          } 
+      }
+      var isTargetCompleted = document.getElementsByClassName(pi + " " + pj)[0].innerHTML.length == 1;
+    
+    Array.from(document.getElementsByClassName("parentTd")).forEach(function (parentTd) {
+     //console.log('before: ' + parentTd.classList);
+     if (parentTd.innerHTML.length > 1 && ((parentTd.classList.contains(pi) && parentTd.classList.contains(pj)) || isTargetCompleted)){
+         parentTd.classList.add("enabled");
+         parentTd.classList.remove("disabled");
+     } else {
+         parentTd.classList.add("disabled");
+         parentTd.classList.remove("enabled");
+     } 
+     //console.log('after: ' + parentTd.classList);
+    });
     turn = turn === 'X' ? 'O' : 'X';
     document.getElementById('turn').textContent = 'turn ' + turn;
-  }
 }
 
 init();
